6b6656d68b77a5a90366ddc52f06954c8c750031
raylee2015 <lilinghui-123@163.com>
2016-11-25 17:12:11 +0800

    分离了spring的配置文件

--- src/main/java/com/esemip/service/TestService.java
+++ src/main/java/com/esemip/service/TestService.java
@@ -1,5 +1,11 @@
 package com.esemip.service;
 
+import java.util.List;
+
+import com.esemip.entity.User;
+
 public interface TestService {
     public String testQuery() throws Exception;
+
+	List<User> queryUsers() throws Exception;
 }
