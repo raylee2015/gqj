6df5b573426e01022344cb9438b51a4f02d97622
raylee2015 <lilinghui-123@163.com>
2016-11-25 17:12:11 +0800

    分离了spring的配置文件

--- src/main/java/com/esemip/controller/TestController.java
+++ src/main/java/com/esemip/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.esemip.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,18 +9,34 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.esemip.entity.User;
 import com.esemip.service.TestService;
 
 @Controller
-@RequestMapping("/testController")
+@RequestMapping("/test")
 public class TestController {
 
 	public static final Logger LOGGER = Logger.getLogger(TestController.class);
 
 	@Autowired
 	private TestService testService;
+
+	@RequestMapping("/showUser")
+	public String toIndex(HttpServletRequest request, Model model) throws Exception {
+		String result = testService.testQuery();
+		model.addAttribute("user", result);
+		return "showUser";
+	}
+
+	@RequestMapping("/showUser2")
+	public String showUser(HttpServletRequest request, Model model) throws Exception {
+		List<User> users = testService.queryUsers();
+		model.addAttribute("users", users);
+		return "showUser";
+	}
 
 	@RequestMapping("/test")
 	public void test(HttpServletRequest request, HttpServletResponse response) {
--- src/main/java/com/esemip/service/impl/TestServiceImpl.java
+++ src/main/java/com/esemip/service/impl/TestServiceImpl.java
@@ -11,27 +11,26 @@ import com.esemip.service.TestService;
 
 @Service
 public class TestServiceImpl implements TestService {
-    
-    @Autowired
-    private TestDao dao;
-    
-    public String testQuery() throws Exception {
-        List<User> users = dao.testQuery();
-//        User u1 = new User("zhangsan", 18);
-//        User u2 = new User("lisi", 19);
-//        User u3 = new User("wangwu", 12);
-//        users.add(u1);
-//        users.add(u2);
-//        users.add(u3);
-//        List<User> users = new ArrayList<User>();
-        String res = "";
-        if (users != null && users.size() > 0) {
-            for (User user : users) {
-                res += user.toString() + "|";
-            }
-        } else {
-            res = "Not found.";
-        }
-        return res;
-    }
+
+	@Autowired
+	private TestDao dao;
+
+	public String testQuery() throws Exception {
+		List<User> users = dao.testQuery();
+		String res = "";
+		if (users != null && users.size() > 0) {
+			for (User user : users) {
+				res += user.toString() + "|";
+			}
+		} else {
+			res = "Not found.";
+		}
+		return res;
+	}
+
+	@Override
+	public List<User> queryUsers() throws Exception {
+		List<User> users = dao.testQuery();
+		return users;
+	}
 }
