9bae0e22e4f756239d0e5f523e8cb1b3fa752b18
Administrator <Administrator@USER-20090101UW>
2016-11-27 21:25:06 +0800

    整合了easyui，并给出一个例子

---
+++ src/main/java/com/base/util/sample/JacksonDemo.java
@@ -0,0 +1,68 @@
+/**   
+* @Title: Test.java 
+* @Package com.base.util 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author A18ccms A18ccms_gmail_com   
+* @date 2016年11月27日 上午11:04:39 
+* @version V1.0   
+*/
+package com.base.util.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.base.util.JacksonUtil;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.sample.entity.Sample;
+
+/**
+ * 实例实现利用jackson实现实体对象与json字符串的互相转换
+ * 
+ * @author liangming.deng
+ * 
+ */
+public class JacksonDemo {
+	public static void main(String[] args) {
+		Sample sample1 = new Sample();
+		sample1.setUserId(1l);
+		sample1.setUserName("llh");
+		sample1.setUserCode("1230");
+		Sample sample2 = new Sample();
+		sample2.setUserId(2l);
+		sample2.setUserName("lyc");
+		sample2.setUserCode("1231");
+
+		List<Sample> Samples = new ArrayList<>();
+		Samples.add(sample1);
+		Samples.add(sample2);
+
+		// 对象转json
+		String SampleToJson = JacksonUtil.toJSon(sample1);
+		System.out.println("---Sample to json--");
+		System.out.println(SampleToJson);
+
+		// json转字符串
+		Sample jsonToSample = JacksonUtil.readValue(SampleToJson, Sample.class);
+		System.out.println();
+		System.out.println("--json to Sample--");
+		System.out.println(jsonToSample.toString());
+
+		// List 转json字符串
+		String listToJson = JacksonUtil.toJSon(Samples);
+		System.out.println();
+		System.out.println("--list to json--");
+		System.out.println(listToJson);
+
+		// 数组json转 List
+		List<Sample> jsonToSamples = JacksonUtil.readValue(listToJson, new TypeReference<List<Sample>>() {
+		});
+		String SampleString = "";
+		for (Sample Sample : jsonToSamples) {
+			SampleString += Sample.toString() + "\n";
+		}
+		System.out.println();
+		System.out.println("--json to Samples--");
+		System.out.println(SampleString);
+	}
+
+}
--- src/main/java/com/sample/controller/SampleController.java
+++ src/main/java/com/sample/controller/SampleController.java
@@ -1,14 +1,17 @@
 package com.sample.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sample.entity.Sample;
 import com.sample.service.ISampleService;
@@ -22,10 +25,19 @@ public class SampleController {
 	@Autowired
 	private ISampleService sampleService;
 
-	@RequestMapping("/sampleTest")
-	public String sampleTest(HttpServletRequest request, Model model) throws Exception {
-		List<Sample> samples = sampleService.querySamples();
-		model.addAttribute("samples", samples);
+	@RequestMapping(value = "/index.do", method = RequestMethod.GET)
+	public String sampleTest() {
 		return "sampleTest";
+	}
+
+	@RequestMapping("/sampleTestList.do")
+	@ResponseBody
+	public Map<String, Object> sampleTestList(HttpServletResponse request, HttpServletResponse response)
+			throws Exception {
+		List<Sample> samples = sampleService.querySamples();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("rows", samples);
+		map.put("total", 20);
+		return map;
 	}
 }
